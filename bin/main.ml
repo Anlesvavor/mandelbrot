3247a46ee71b7c7bd346d0192f156f002f5e9dca
Anlesvavor <jaredmaster32@gmail.com>
2023-08-09 23:36:35 -0500

    Fit window to bounds of the image

--- bin/main.ml
+++ bin/main.ml
@@ -13,26 +13,77 @@ let mandelbrot_canvas =
       List.map (fun y -> x, y) (Mandelbrot.range y1 y2 step)
     ) (Mandelbrot.range x1 x2 step)
   |> List.flatten
-  |> List.map (fun (re, im) -> ((re, im), Mandelbrot.is_in_mandelbrot {re ; im}))
+  |> List.map (fun (re, im) -> ((re, im), Mandelbrot.is_in_mandelbrot {re; im}))
+;;
+
+module BoundingRect = struct
+  type t =
+    { min_x : float
+    ; max_x : float
+    ; min_y : float
+    ; max_y : float
+    }
+
+  let init
+      ?(min_x = Float.max_float)
+      ?(max_x = Float.min_float)
+      ?(min_y = Float.max_float)
+      ?(max_y = Float.min_float)
+      ()
+    : t =
+    { min_x; max_x; min_y; max_y }
+  ;;
+
+  let print (t : t) =
+    print_string @@ Printf.sprintf "{min_x = %f; max_x = %f; min_y = %f; max_y: %f}"
+      t.min_x t.max_x t.min_y t.max_y
+  ;;
+end
+
+let bounding_rect (coords : (float * float) list) : BoundingRect.t =
+  let rec aux (bounding_rect : BoundingRect.t) list = match list with
+    | [] -> bounding_rect
+    | (x, y) :: xs ->
+      let min_x = if x < bounding_rect.min_x then x else bounding_rect.min_x in
+      let min_y = if y < bounding_rect.min_y then y else bounding_rect.min_y in
+      let max_x = if x > bounding_rect.max_x then x else bounding_rect.max_x in
+      let max_y = if y > bounding_rect.max_y then y else bounding_rect.max_y in
+      aux (BoundingRect.init ~min_x ~max_x ~min_y ~max_y ()) xs
+  in
+  aux (BoundingRect.init ()) coords
+;;
+
+mandelbrot_canvas
+|> List.filter (fun (_, b) -> b)
+|> List.map (fun ((re, im), _) -> (re, im))
+|> bounding_rect
+|> BoundingRect.print
+;;
 
 let main () =
-  let width = int_of_float @@ (x2 -. x1) /. step in
-  let height = int_of_float @@ (y2 -. y1) /. step in
+  let b_rect =
+    mandelbrot_canvas
+    |> List.filter (fun (_, b) -> b)
+    |> List.map (fun ((re, im), _) -> (re, im))
+    |> bounding_rect
+  in
+  let width = int_of_float @@ (b_rect.max_x -. b_rect.min_x) /. step in
+  let height = int_of_float @@ (b_rect.max_y -. b_rect.min_y) /. step in
   let open Graphics in
   let resolution = Printf.sprintf " %dx%d" width height in
   open_graph resolution;
   set_window_title @@ Printf.sprintf "Mandelbrot set: %s" resolution;
   mandelbrot_canvas
-  |> List.map (fun ((x, y), v) ->
-      let offset_x = 2.0 /. step in
-      let offset_y = 1.0 /. step in
+  |> List.map (fun ((x, y), b) ->
+      let offset_x = (-.b_rect.min_x) /. step in
+      let offset_y = (-.b_rect.min_y) /. step in
       let x' = int_of_float @@ (x /. step +. offset_x) in
       let y' = int_of_float @@ (y /. step +. offset_y) in
-      ((x', y'), v))
-  |> List.iter (fun ((x, y), v) ->
-      if v
-      then set_color (rgb 0 0 0)
-      else set_color (rgb 255 255 255)
+      ((x', y'), b))
+  |> List.iter (fun ((x, y), b) ->
+      if b
+      then set_color @@ rgb 0 0 0
+      else set_color @@ rgb 255 255 255
       ;
       plot x y
     )
@@ -41,3 +92,4 @@ let main () =
 ;;
 
 let _ = main ()
+;;
--- lib/mandelbrot.ml
+++ lib/mandelbrot.ml
@@ -9,7 +9,7 @@ let is_in_mandelbrot (c : Complex.t) : bool =
   let mandelbrot' = mandelbrot c in
   let rec is_in_mandelbrot' fitness i c z =
     if fitness = 0
-    then (* function "tired" of searching, c must be part of the set *) true
+    then true (* function "tired" of searching, c must be part of the set *)
     else
       let z' = mandelbrot' i z in
       if Complex.norm z > 2.0
@@ -17,23 +17,6 @@ let is_in_mandelbrot (c : Complex.t) : bool =
       else is_in_mandelbrot' (pred fitness) (succ i) c z'
   in
   is_in_mandelbrot' 25 0 c Complex.zero
-;;
-
-let rec mandelbrot_seq (c : Complex.t) ?(i : int = 0) (z : Complex.t) : Complex.t Seq.node =
-  let z = mandelbrot c i z in
-  Seq.cons z (fun () -> mandelbrot_seq c ~i:(succ i) z) ()
-;;
-
-let head_or_fail = function
-  | [] -> failwith "head_of_fail: fail!"
-  | x :: _ -> x
-;;
-
-let iter_before_exceeding (value : Complex.t) (seq : unit -> Complex.t Seq.node) : int =
-  seq
-  |> Seq.take 10
-  |> Seq.take_while (fun el -> el < value)
-  |> Seq.length
 ;;
 
 let range (lower_bound : float) (upper_bound : float) (step : float) =
@@ -49,10 +32,10 @@ let range (lower_bound : float) (upper_bound : float) (step : float) =
       | Direction.Upwards -> step
       | Direction.Downwards -> -.step
     in
-    let current_position' = current_position +. step' in
-    if current_position' > upper_bound
+    (* let current_position' = current_position +. step' in *)
+    if current_position > upper_bound
     then acc
-    else aux (current_position' :: acc) current_position'
+    else aux (current_position :: acc) (current_position +. step')
   in
   aux [] lower_bound
   |> List.rev
